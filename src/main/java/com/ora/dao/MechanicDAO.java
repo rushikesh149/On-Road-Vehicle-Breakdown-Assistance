b3a844e8cb0df58d451c11ec35adb1109df6554a
Rushikesh <Rushikesh@sai-pc>
2020-12-09 18:58:12 +0530

    add

--- src/main/java/com/ora/dao/MechanicDAO.java
+++ src/main/java/com/ora/dao/MechanicDAO.java
@@ -2,9 +2,16 @@ package com.ora.dao;
 
 import java.util.List;
 
+
 import com.ora.entity.Mechanic;
 
+
 public interface MechanicDAO {
+	
+	/*
+	 * 
+	 RushikeshZunjarrao*/
+	
 	public List viewMechanicDetails(Mechanic m);
 	public List<Mechanic> viewRequest(int mechanicId);
 	public List<Mechanic> viewFeedback(int mechanicId);
